678a27bb0bf275630b1619dafa7530cf3050e7fb
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2026-01-24 00:32:06 +0800

    feat: jest

--- src/transaction/transaction.service.spec.ts
+++ src/transaction/transaction.service.spec.ts
@@ -1,18 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TransactionService } from './transaction.service';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { TransactionRepository } from './transaction.repository';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
-describe('TransactionService', () => {
-  let service: TransactionService;
+@Injectable()
+export class TransactionService {
+  constructor(private readonly transactionRepo: TransactionRepository) {}
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService],
-    }).compile();
+  getAllTransactions() {
+    return this.transactionRepo.findAll();
+  }
 
-    service = module.get<TransactionService>(TransactionService);
-  });
+  getTransactionById(id: number) {
+    return this.transactionRepo.findOne(id);
+  }
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  getTransactionsByUserId(userId: number) {
+    return this.transactionRepo.findByUserId(userId);
+  }
+
+  createTransaction(data: { userId: number; total: number | string }) {
+    if (typeof data.total === 'number' && data.total <= 0) {
+      throw new BadRequestException('Total must be greater than 0');
+    }
+    return this.transactionRepo.createTransaction(data);
+  }
+
+  update(id: number, data: UpdateTransactionDto) {
+    return this.transactionRepo.update(id, data);
+  }
+
+  delete(id: number) {
+    return this.transactionRepo.delete(id);
+  }
+}
+
+then what does this do
